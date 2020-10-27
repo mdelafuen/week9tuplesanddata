4cd43df74ab9ab71edc102d0a418c37185e21c55
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-10-27 14:58:52 -0400

    1

--- OpenPyXl.py
+++ OpenPyXl.py
@@ -6,7 +6,10 @@ def examine_income_data(excel_file_name):
     workbook_file = openpyxl.load_workbook(excel_file_name)
     worksheet = workbook_file.active
     for current_row in worksheet.rows:
-        pass
+        state_cell = current_row[0]
+        state_name = state_cell.value
+        median_income2018 = current_row[1].value
+        print(f"{state_name} \t: {median_income2018}")
 
 def main():
     examine_income_data("CensuseMedianIncome.xlsx")
